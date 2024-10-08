804bd0b7d569eaa8cc44e103cab8b8a48444ade6
João Lucas Melo de Oliveira <joao.oliveira307@fatec.sp.gov.br>
2024-10-07 22:32:02 -0300

    Att

--- Projeto/fatec-orders/src/components/HOCS/withDataFetching.tsx
+++ Projeto/fatec-orders/src/components/HOCS/withDataFetching.tsx
@@ -1,26 +1,54 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Alert, AlertTitle, CircularProgress } from "@mui/material";
 
 export const withDataFetching = (url: string) => (WrappedComponent: any) => {
   return function WithDataFetching(props: any) {
     const [data, setData] = useState();
     const [error, setError] = useState<string>();
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
     useEffect(() => {
-      console.log("slug: ", props.params.slug);
       const fetchData = async () => {
         const id = props.params?.slug ? `/${props.params?.slug}` : "";
 
         try {
           const response = await axios.get(`${url}${id}`);
           setData(response.data);
-        } catch (error: any) {
-          console.log("Erro: ", error);
-          setError(error.message);
+        } catch (error) {
+          setError("Erro ao tentar realizar a consulta.");
+        } finally {
+          setIsLoading(false);
         }
       };
       fetchData();
     }, [props.params.slug]);
 
-    return <WrappedComponent {...props} data={data} error={error} />;
+    return (
+      <>
+        {error ? (
+          <Alert
+            variant="filled"
+            severity="error"
+            sx={{
+              marginTop: "70px",
+            }}
+          >
+            <AlertTitle>Ocorreu um erro 😥</AlertTitle>
+            {error}
+          </Alert>
+        ) : undefined}
+        <WrappedComponent {...props} data={data} />
+        {isLoading ? (
+          <CircularProgress
+            sx={{
+              position: "absolute",
+              top: "calc(100vh/2)",
+              left: "calc(100vw/2)",
+            }}
+          />
+        ) : undefined}
+      </>
+    );
   };
 };
