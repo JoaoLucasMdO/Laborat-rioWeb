746311b6df3a7e549d9746ec07459268ebbd2800
João Lucas Melo de Oliveira <joao.oliveira307@fatec.sp.gov.br>
2024-10-14 21:53:38 -0300

    Att

--- Projeto/fatec-orders/tests/pages/products/Products.spec.tsx
+++ Projeto/fatec-orders/tests/pages/products/Products.spec.tsx
@@ -1,20 +1,47 @@
 import Products from "@/app/products/page";
 import { render, screen } from "@testing-library/react";
 import mockRouter from "next-router-mock";
+import { setupServer } from "msw/node";
+import { env } from "@/config/env";
+import { http } from "msw";
 
 jest.mock("next/navigation", () => require("next-router-mock"));
+
+const server = setupServer(
+  http.get(`${env.apiBaseUrl}/produto`, () => {
+    return Response.json({
+      produtos: [
+        {
+          id: 1,
+          descricao: "Bolacha",
+          marca: "Tortuguita",
+          varlo: 1.99,
+          peso_gramas: 100,
+          sabor: "morango",
+        },
+      ],
+    });
+  })
+);
 
 describe("Products list page", () => {
   beforeAll(() => {
     mockRouter.setCurrentUrl("/products");
+    server.listen();
+  });
+
+  afterAll(() => {
+    server.close();
   });
 
   it("Should render products list", () => {
     render(<Products />);
 
-    screen.findByRole("cell", {
-      name: /dolly/i,
+    const productName = screen.findByRole("cell", {
+      name: /Tortuguita/i,
     });
+
+    console.log(productName);
 
     screen.logTestingPlaygroundURL();
   });
